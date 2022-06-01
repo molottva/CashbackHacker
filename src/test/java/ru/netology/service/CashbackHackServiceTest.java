700f54b60bebeb8d361e9e97823e6d44e40998cf
molottva <molottva@list.ru>
2022-06-01 15:58:58 +0300

    Add JUnit4 tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,46 @@
+package ru.netology.service;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @org.junit.Test
+    public void testRemainBoundaryValuesOne() {
+        org.junit.Assert.assertThrows(RuntimeException.class, () -> {
+            service.remain(-1);
+        });
+    }
+
+    @org.junit.Test
+    public void testRemainBoundaryValuesTwo() {
+        org.junit.Assert.assertEquals(1000, service.remain(0));
+    }
+
+    @org.junit.Test
+    public void testRemainBoundaryValuesThree() {
+        org.junit.Assert.assertEquals(999, service.remain(1));
+    }
+
+    @org.junit.Test
+    public void testRemainBoundaryValuesFour() {
+        org.junit.Assert.assertEquals(1, service.remain(999));
+    }
+
+    @org.junit.Test
+    public void testRemainBoundaryValuesFive() {
+        org.junit.Assert.assertEquals(0, service.remain(1000));
+    }
+
+    @org.junit.Test
+    public void testRemainBoundaryValuesSix() {
+        org.junit.Assert.assertThrows(RuntimeException.class, () -> {
+            service.remain(1001);
+        });
+    }
+
+    @org.junit.Test
+    public void testRemainCriticalPath() {
+        org.junit.Assert.assertEquals(600, service.remain(400));
+    }
+}
