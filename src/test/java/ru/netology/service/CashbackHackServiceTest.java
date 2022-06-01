f45dbe9cac2ac2fad8ca575abb0e4eb070041d5b
molottva <molottva@list.ru>
2022-06-01 16:46:21 +0300

    Added JUnit Platform tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -43,4 +43,43 @@ public class CashbackHackServiceTest {
     public void testRemainCriticalPath() {
         org.junit.Assert.assertEquals(600, service.remain(400));
     }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainBoundaryValuesOneWithJupiter() {
+        org.junit.jupiter.api.Assertions.assertThrows(RuntimeException.class, () -> {
+            service.remain(-1);
+        });
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainBoundaryValuesTwoWithJupiter() {
+        org.junit.jupiter.api.Assertions.assertEquals(1000, service.remain(0));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainBoundaryValuesThreeWithJupiter() {
+        org.junit.jupiter.api.Assertions.assertEquals(999, service.remain(1));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainBoundaryValuesFourWithJupiter() {
+        org.junit.jupiter.api.Assertions.assertEquals(1, service.remain(999));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainBoundaryValuesFiveWithJupiter() {
+        org.junit.jupiter.api.Assertions.assertEquals(0, service.remain(1000));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainBoundaryValuesSixWithJupiter() {
+        org.junit.jupiter.api.Assertions.assertThrows(RuntimeException.class, () -> {
+            service.remain(1001);
+        });
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainCriticalPathWithJupiter() {
+        org.junit.jupiter.api.Assertions.assertEquals(600, service.remain(400));
+    }
 }
